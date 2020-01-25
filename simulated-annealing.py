67cd7fcbadaa8e04195b362b924fd38380631fc3
ked-m <ked-mic@wp.pl>
2020-01-25 20:05:41 +0100

    simulated annealing

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -1,19 +1,20 @@
 import pandas as pd
-import xlrd
 import numpy as np
 import random as rd
 import sklearn
+import xlrd
 
 df = pd.read_excel('data.xlsx')
 df = sklearn.utils.shuffle(df)
 df = df.reset_index(drop=True)
 np.set_printoptions(suppress=True)
 df = df.to_numpy()
-count_rows = df.shape[0]
+tasks = df.shape[0]
+number_of_machines = df.shape[1]
 
 
 def calculate(arr):
-    first_row = arr[0, 0:11]
+    first_row = arr[0, 0:number_of_machines]
     w = 0
     row_sum = np.array([])
     for j in first_row:
@@ -21,7 +22,7 @@ def calculate(arr):
             row_sum = np.append(row_sum, j)
         elif w == 1:
             row_sum = np.append(row_sum, j)
-        elif w < 11:
+        elif w < number_of_machines+1:
             row_sum = np.append(row_sum, j + row_sum[w - 1])
         w = w + 1
     result = row_sum
@@ -29,7 +30,7 @@ def calculate(arr):
     if arr.ndim > 1:
         row_before = result
         number_of_tasks = arr.shape[0]
-        df_copy = arr[1:number_of_tasks, 0:11]
+        df_copy = arr[1:number_of_tasks, 0:number_of_machines]
         n = 0
         for i in df_copy:
             row_add = np.array([])
@@ -38,13 +39,13 @@ def calculate(arr):
                     row_add = np.append(row_add, k)
                 elif n == 1:
                     row_add = np.append(row_add, k + row_before[1])
-                elif n < 11:
+                elif n < number_of_machines+1:
                     add = max(row_add[n - 1], row_before[n]) + k
                     row_add = np.append(row_add, add)
                 else:
                     pass
                 n = n + 1
-                if n == 11:
+                if n == number_of_machines:
                     n = 0
                     row_before = row_add.copy()
             result = np.vstack([result, row_add])
@@ -58,17 +59,17 @@ def swap(arr, a, b):
 
 def sasiedztwo():
     list = []
-    while len(list)<6: #maksymalna dlugosc sasiedztwa
-        random = rd.randint(0, count_rows-1)
+    while len(list) < 5: #maksymalna dlugosc sasiedztwa
+        random = rd.randint(0, tasks - 1)
         if random not in list:
             list.append(random)
     return list
 
 
-temp = 11
+temp = 100
 def templow():
     global temp
-    temp =temp * 0.99
+    temp = temp * 0.999
 
 
 def main(data):
@@ -96,11 +97,14 @@ def main(data):
     else:
         return data_copy2
 
-for i in range(1000):
+for i in range(2000):
     final = main(df)
     df = final.copy()
-print(df)
 
+print(calculate(final))
+final = pd.DataFrame(calculate(final))
+final = final.astype(int)
+final.to_csv("simulated-annealing_result.csv", index=False, header=None)
 
 
 
